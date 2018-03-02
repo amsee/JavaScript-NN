1ff95509d8b9837d9d2a374113e2ddfb58f4d17f
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-02 16:40:10 -0500

    If Statements

---
+++ IfStatements.js
@@ -0,0 +1,30 @@
+// todo: JS-12 IF STATEMENTS 
+// *************************
+// todo IF you pay for your train ticket...
+// you won't get a fine
+// todo IF you otake a crap in public...
+// you'll wind up in prison
+
+// * ---ASSIGN--- variable to true
+var youlikeMeat = true;
+
+// * ---CHECK--- to see if expressione evaluates as true
+if (youlikeMeat) //? this will evaluate as true
+//  ? code block
+{ 
+// * ---DISPLAY--- out to user if statement is true
+    document.write("Here is the meaty menu...");
+}
+
+// * PRINTS
+var youlikeMeat = false;
+if (7 > 5)
+{
+    document.write("this expression is true");
+}
+
+// * DOES NOT PRINT
+if (7 < 5)
+{
+    document.write("this expression is true");
+}
