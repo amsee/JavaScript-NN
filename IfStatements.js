8eb4d06da2372c3564502d3b383c2c125d1a4f14
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-02 16:49:26 -0500

    #12 - If Statements

--- IfStatements.js
+++ IfStatements.js
@@ -28,3 +28,35 @@ if (7 < 5)
 {
     document.write("this expression is true");
 }
+
+var youlikeMeat = false;
+var myNum = 10;
+
+if (myNum > 10) //! FALSE will not print out; 10 is not greater than 10.
+{
+    document.write("myNum is greatert than 10");
+}
+
+if (myNum > 9) //! TRUE will print; 10 is greater than 9.
+{
+    document.write("myNum is greater than 10");
+}
+
+if (myNum == 10) //! TRUE will print; 10 equates to 10.
+{
+    document.write("myNum is equal than 10");
+// * if true, execute all above, if not: 
+} else{
+    document.write("myNum is not equal to 10");
+
+}
+
+var youlikeMeat = false;
+var myNum = 10;
+
+if (youlikeMeat)
+{
+    document.write("you like meat");
+} else {
+    document.write("you hate meat"); //! this is the output because we have initialized the variable to false
+}
