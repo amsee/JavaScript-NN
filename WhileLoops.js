4c8c28ce68fde079df2b49ae2b03922bcb6cf941
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-05 14:10:43 -0500

    #16 - While Loops

---
+++ WhileLoops.js
@@ -0,0 +1,12 @@
+// todo: JS-16 [WHILE LOOPS]
+
+var age = 5;
+
+while (age < 10) {
+
+    console.log("Your age is less than 10");
+    age++; //? add 1 to the loop each time
+
+}
+
+document.write("you are now over 10")
