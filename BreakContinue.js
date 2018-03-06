a562855ec644d9e07a01c81c1de42be99ac1a84e
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-06 15:02:07 -0500

    #18 - Break and Continue

--- BreakContinue.js
+++ BreakContinue.js
@@ -5,7 +5,7 @@ for (i = 0; i < 10; i++) {
     if (i === 5 || i === 3){
         
         continue; //? keyword is similar to break ("skip out the code for this iteration of code")
-        // ? weak form of break. don't print, just go back to the top
+        // ? weak form of break. don't print, just go back to the top3
     }
 
     console.log(i);
