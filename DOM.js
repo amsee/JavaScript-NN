bd894aa6aed3d20cf3fa781b017b9b6bdf8d4860
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-08 14:36:40 -0500

    #32 - What is the DOM in JavaScript?

---
+++ DOM.js
@@ -0,0 +1,14 @@
+// todo: JS-32 --- THE DOM (DOCUMENT OBJECT MODEL)
+// * The DOM is an 'application programming interface'
+// * Use the DOM when we interact with web pages
+    // * Add content to a HTML document
+    // * Delete content from a HTML document    
+    // * Change Content on a HTML document 
+        // * The document (HTML document) is just the web page 
+// todo: Objects Are Elements
+// * Every HTML element in the document is an object
+    // * <head></head> is an object
+    // * <body></body> is an object
+    // * <ul></ul> is an object
+    // * <p></p> is an object
+
