7a3a86f574033252bc0d723a1401f24589f5f6e9
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-13 09:39:01 -0400

    #44 - Very Simple Form Validation

---
+++ OnclickEvent.js
@@ -0,0 +1,10 @@
+// todo: JS-40 --- THE onCLICK EVENT
+
+// * 2 variables storing these 2 elements
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = fucntion(){
+    if(content.className == "open")
+        // shrink the box
+};
