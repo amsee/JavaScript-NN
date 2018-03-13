b5b74b1ad69caa933587a54df027205e41f35140
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-13 10:02:31 -0400

    #45 - JavaScript Libraries

---
+++ Libraries.js
@@ -0,0 +1,11 @@
+// todo: JS-45 --- LIBRARIES
+    // * General purpose
+    // * Utilize other people's code
+    // * Animation
+    // * Form enhancement
+    // * Video
+    // * 2 - 3 lines of code vs 100+
+    // * Many more...
+
+var myPara = document.getElementById("content").getElementsByTagName("p"[5]);
+var myPara2 = $("#content p:last-child");
